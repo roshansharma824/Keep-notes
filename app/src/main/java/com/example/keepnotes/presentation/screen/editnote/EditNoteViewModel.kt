60b10057a71c3f5e3e95417fd0aad967a3b7e1d1
Roshan Kumar Sharma <roshan@conscent.ai>
2024-03-14 13:07:47 +0530

    edit note functionality done

--- app/src/main/java/com/example/keepnotes/presentation/screen/editnote/EditNoteViewModel.kt
+++ app/src/main/java/com/example/keepnotes/presentation/screen/editnote/EditNoteViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.keepnotes.presentation.screen.editnote
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.keepnotes.domain.model.Note
@@ -8,7 +12,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,6 +22,17 @@ class EditNoteViewModel @Inject constructor(
 
     private val _getNote = MutableStateFlow<Note>(Note(-1,"",""))
     val getNote = _getNote.asStateFlow()
+    private var note by mutableStateOf("")
+    private var title by mutableStateOf("")
+
+    fun updateNote(input: String) {
+        note = input
+    }
+    fun updateTitle(input: String) {
+        title = input
+    }
+
+
 
     fun getNote(id:Int) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -33,7 +47,8 @@ class EditNoteViewModel @Inject constructor(
         useCases.addNoteUseCase.invoke(note)
     }
 
-    fun updateNote(note: Note) = viewModelScope.launch {
-        useCases.updateNoteUseCase.invoke(note)
+    fun updateNote() = viewModelScope.launch {
+        val updateNote = Note(id = getNote.value.id, title = title.ifBlank { getNote.value.title }, note = note.ifBlank { getNote.value.note })
+        useCases.updateNoteUseCase.invoke(updateNote)
     }
 }
