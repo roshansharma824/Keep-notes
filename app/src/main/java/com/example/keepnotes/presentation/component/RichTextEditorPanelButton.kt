d6ab7563e92a9ee52b2c67c0c701780bf4d468ff
Roshan Kumar Sharma <roshan@conscent.ai>
2024-05-08 21:12:43 +0530

    re-name components

--- app/src/main/java/com/example/keepnotes/presentation/component/RichTextEditorPanelButton.kt
+++ app/src/main/java/com/example/keepnotes/presentation/component/RichTextEditorPanelButton.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SlackDemoPanelButton(
+fun KeepNotePanelButton(
     onClick: () -> Unit,
     icon: ImageVector,
     tint: Color? = null,
