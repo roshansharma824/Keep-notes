94f93d75c5011cc5b31ed8c9098dbf6ce4f3d6cb
Roshan kumar sharma <85518522+roshansharma824@users.noreply.github.com>
2024-05-31 21:53:43 +0530

    Revert "search bug fix"

--- app/src/main/java/com/example/keepnotes/navigation/graph/MainNavGraph.kt
+++ app/src/main/java/com/example/keepnotes/navigation/graph/MainNavGraph.kt
@@ -34,18 +34,12 @@ fun MainNavGraph(navHostController: NavHostController = rememberNavController())
             startDestination = BottomNavItemScreen.Home.route
         ) {
             composable(route = BottomNavItemScreen.Home.route) {
-                RootScreen(
-                    navController = navHostController,
-                    sharedTransitionScope = this@SharedTransitionLayout,
-                    animatedContentScope = this@composable
+                RootScreen(navController = navHostController,
+                    sharedTransitionScope = this@SharedTransitionLayout, animatedContentScope = this@composable
                 )
             }
             composable(route = BottomNavItemScreen.Search.route) {
-                SearchNotesScreen(
-                    navController = navHostController,
-                    sharedTransitionScope = this@SharedTransitionLayout,
-                    animatedContentScope = this@composable
-                )
+                SearchNotesScreen(navController = navHostController)
             }
             composable(route = BottomNavItemScreen.CheckListNote.route) {
                 CheckListNote()
@@ -86,7 +80,7 @@ fun NavGraphBuilder.detailsNavGraph(
                     navController = navHostController,
                     noteId = noteId,
                     animatedContentScope = this@composable,
-                )
+                    )
             }
         }
     }
